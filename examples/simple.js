737ed5267243568c473ff688a468498f83765258
Benson Wong <mostlygeek@gmail.com>
2011-02-02 22:02:28 -0800

    adding examples

---
+++ examples/simple.js
@@ -0,0 +1,9 @@
+/* 
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+var App = new Extendable({                          // <1>
+    me : 'MyApp'
+});
+
